d7bc987c617c52a1cd863ec753b2a5a684a92556
Bryan Hill <bf361640-7eb8-4e29-80f7-5250fa6b442b@aleeas.com>
2022-12-31 10:20:16 -0700

    Include directory instead of individual files

--- src/ProjectEuler100.jl
+++ src/ProjectEuler100.jl
@@ -5,12 +5,12 @@ using Base.Iterators
 include("lib/lib.jl")
 using .ProjectEulerLibrary
 
-# include.(filter(contains(r".jl$"), readdir(dirname(@__FILE__) * "/problems"; join=true)))
+include.(filter(contains(r".jl$"), readdir(dirname(@__FILE__) * "/problems"; join=true)))
 
-include("problems/pb001.jl")
-include("problems/pb002.jl")
-include("problems/pb003.jl")
-include("problems/pb004.jl")
-include("problems/pb005.jl")
+# include("problems/pb001.jl")
+# include("problems/pb002.jl")
+# include("problems/pb003.jl")
+# include("problems/pb004.jl")
+# include("problems/pb005.jl")
 
 end
