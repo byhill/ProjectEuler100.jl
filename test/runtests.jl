20197787939a05d15f26f9017cb47493a2487b5c
byhill <byhill.xi1ej@dralias.com>
2023-02-05 04:51:28 -0700

    Delete txt folder from test directory

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,7 @@ using Test
 include("solutions.jl")
 
 @testset "ProjectEuler100.jl" begin
+    cd("..")
     for key in sort(collect(keys(SOLUTIONS)))
         @testset "Problem $key" begin
             f = getfield(Main, Symbol("problem$key"))
