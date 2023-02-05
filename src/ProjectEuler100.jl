82880aa6d8494faf4a5bb6da7473c5f9241765f4
byhill <byhill.xi1ej@dralias.com>
2023-02-05 00:49:22 -0700

    Explicitly include all problems

--- src/ProjectEuler100.jl
+++ src/ProjectEuler100.jl
@@ -7,25 +7,6 @@ using Primes: isprime, primes
 include("lib/lib.jl")
 using .ProjectEulerLibrary
 
-##
-export cartesianproduct
-
-export bezouts, pells_equation_fundamental_solution
-
-export sumto, sumsquaresto, ilog, concat, undigits
-export ispalindrome, ispandigital, issquare, istriangular, ispentagonal
-export digit_factorial
-
-export σₓ, totients
-
-export nthprime_upperbound
-##
-
-
-# Includes all files in the /problems directory.
-# You probably shouldn't do this for an actual project
-# (for one, the lsp does not work correctly if you do this),
-# but it is fine for just a collection of code snippets.
-include.(filter(contains(r".jl$"), readdir(dirname(@__FILE__) * "/problems"; join=true)))
+include("problems.jl")
 
 end
