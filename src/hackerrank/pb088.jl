0091cee5601d6eb4567bfc510e50e83c91a04572
byhill <byhill.xi1ej@dralias.com>
2024-05-13 15:46:00 -0600

    Add problem088

--- src/hackerrank/pb088.jl
+++ src/hackerrank/pb088.jl
@@ -1,18 +1,7 @@
-module Problem088
-
 const minimial_productsums = Int[]
 
 
-"""
-    problem088()
-
-Problem 088 of Project Euler.
-
-https://projecteuler.net/problem=088
-
-If n = n1 ⋅ n2 ⋯ n_l, then n = n1 + n2 + ⋯ + n_l + (n - n1 - n2 - ⋯ - n_l)⋅1.
-"""
-function problem088(K::Integer=12000)
+function problem088(K::Integer)
     resize!(minimial_productsums, K)
     fill!(minimial_productsums, typemax(Int))
     minimial_productsums[1] = 0
@@ -25,18 +14,16 @@ end
 
 function productsum(p, s, l, a, K)
     l += 1
-    for n in a:2K÷p
+    for n in a:div(2K, p)
         product = p * n
         sum = s + n
         k = product - sum + l
-        k ≤ K || break
+        k <= K || break
         minimial_productsums[k] = min(minimial_productsums[k], product)
         productsum(product, sum, l, n, K)
     end
 end
 
 
-export problem088
-end  # module Problem088
-using .Problem088
-export problem088
+N = parse(Int, readline())
+println(problem088(N))
